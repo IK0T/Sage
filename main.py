52e9a9fd59df02bdced227533813e3a21a84f369
IK0T <94976729+IK0T@users.noreply.github.com>
2022-08-21 13:17:30 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from openpyxl import load_workbook
+
+# Загружаем файл и переходим на первую страницу
+wb = load_workbook('1.xlsx')
+sheet = wb.get_sheet_by_name(wb.get_sheet_names()[0])
+f = list(sheet.values)
+# Удалим все нечетные числа
+f = [[None if f[i][j] % 2 else f[i][j] for i in range(8)] for j in range(8)]
+print(*f, sep='\n')
+
+for i in range(1, len(f) + 1):
+    for j in range(1, len(f[0]) + 1):
+        sheet.cell(row=i, column=j).value = f[i-1][j-1]
+wb.save('2.xlsx')
