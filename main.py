09034adf390a66c47bf7ddf093a7a8b2480e326e
IK0T <94976729+IK0T@users.noreply.github.com>
2022-08-21 13:22:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ wb = load_workbook('1.xlsx')
 sheet = wb.get_sheet_by_name(wb.get_sheet_names()[0])
 f = list(sheet.values)
 # Удалим все нечетные числа
-f = [[None if f[i][j] % 2 else f[i][j] for i in range(8)] for j in range(8)]
+f = [[None if f[i][j] % 2 else f[i][j] for i in range(sheet.max_column)] for j in range(sheet.max_row)]
 print(*f, sep='\n')
 
 for i in range(1, len(f) + 1):
